71eed643e696a647a2d9793c31084e0871c7b674
Sam Schott <ss2151@cam.ac.uk>
2021-03-18 16:35:43 +0000

    add privileged helper tool stubs

---
+++ macOS/Xcode/Maestral/com.samschott.maestral.Helper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  com.samschott.maestral.Helper
+//
+//  Created by Sam Schott on 18/03/2021.
+//  Copyright © 2021 Sam Schott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
